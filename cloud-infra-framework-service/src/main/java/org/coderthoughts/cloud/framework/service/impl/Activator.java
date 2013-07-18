9a6f8f3a90660180855c3bb988fa0381507d9a98
David Bosschaert <david.bosschaert@gmail.com>
2013-07-18 11:27:38 +0100

    Work in progress

--- cloud-infra-framework-service/src/main/java/org/coderthoughts/cloud/framework/service/impl/Activator.java
+++ cloud-infra-framework-service/src/main/java/org/coderthoughts/cloud/framework/service/impl/Activator.java
@@ -35,13 +35,19 @@ public class Activator implements BundleActivator {
             System.setProperty(OSGI_FRAMEWORK_UUID, uuid);
         }
 
+        String [] gearTypeParts = System.getenv("OPENSHIFT_GEAR_TYPE").split("[-]");
+        if (gearTypeParts.length != 2) {
+            throw new Exception("Environment variable OPENSHIFT_GEAR_TYPE does not follow the syntax 'type-version'");
+        }
+
         Hashtable<String, Object> props = new Hashtable<String, Object>();
         props.put(OSGI_FRAMEWORK_UUID, uuid);
-        props.put("org.coderthoughts.framework.ip", publicDNS);
-        props.put("org.coderthoughts.cloud.name", "Red Hat Openshift/DIY/OSGi"); // TODO obtain from underlying cloud
-        props.put("org.coderthoughts.cloud.version", "0.9"); // TODO obtain from underlying cloud
-        props.put("org.coderthoughts.cloud.country", "USA"); // TODO obtain from underlying platform
-        props.put("org.coderthoughts.cloud.location", "US-KS"); // TODO obtain from underlying platform
+        props.put("org.osgi.node.host", publicDNS);
+        props.put("org.osgi.node.host.internal", System.getenv("OPENSHIFT_INTERNAL_IP"));
+        props.put("org.osgi.node.type", gearTypeParts[0]);
+        props.put("org.osgi.node.version", gearTypeParts[1]);
+        props.put("org.osgi.node.country", "USA"); // TODO obtain from underlying platform
+        props.put("org.osgi.node.location", "US-KS"); // TODO obtain from underlying platform
         props.put(Constants.FRAMEWORK_VERSION, context.getProperty(Constants.FRAMEWORK_VERSION));
         props.put(Constants.FRAMEWORK_PROCESSOR, context.getProperty(Constants.FRAMEWORK_PROCESSOR));
         props.put(Constants.FRAMEWORK_OS_NAME, context.getProperty(Constants.FRAMEWORK_OS_NAME));
--- cloud-infra-framework-service/src/main/java/org/coderthoughts/cloud/framework/service/impl/FrameworkNodeStatusImpl.java
+++ cloud-infra-framework-service/src/main/java/org/coderthoughts/cloud/framework/service/impl/FrameworkNodeStatusImpl.java
@@ -35,7 +35,6 @@ class FrameworkNodeStatusImpl implements FrameworkNodeStatus {
         } catch (InvalidSyntaxException e) {
             throw new RuntimeException(e);
         }
-        // return frameworkService.getFrameworkVariables().keySet().toArray(new String[] {});
     }
 
     @Override
@@ -80,72 +79,6 @@ class FrameworkNodeStatusImpl implements FrameworkNodeStatus {
             throw new RuntimeException(e);
         }
 
-        /*
-        for (String filter : filters) {
-            // we only have one variable right now...
-            if (FV_AVAILABLE_MEMORY.matches(filter)) {
-                m.put(FV_AVAILABLE_MEMORY, "" + Runtime.getRuntime().freeMemory());
-            }
-        } */
         return m;
     }
-
-    /*
-    @Override
-    public String[] listServiceVariableNames(long id) {
-        List<String> names = new ArrayList<String>();
-        for (Tuple t : frameworkService.getServiceVariables().keySet()) {
-            if (t.serviceIDs.contains(new Long(id))) {
-                names.add(t.serviceVariableName);
-            }
-        }
-        return names.toArray(new String[] {});
-    }
-
-    @Override
-    public String getServiceVariable(long serviceID, String name) {
-        System.out.println("*** Obtaining info: " + name + " for " + serviceID + " requester IP:" + client);
-
-        try {
-            ServiceReference[] additionRefs =
-                    bundleContext.getServiceReferences(FrameworkStatusAddition.class.getName(),
-                    "(&(" + FrameworkStatusAddition.SERVICE_VARIABLES_KEY + "=" + name + ")" +
-            		"(" + FrameworkStatusAddition.SERVICE_IDS_KEY + "=" + serviceID + "))");
-            if (additionRefs != null) {
-                sortServiceReferences(additionRefs);
-                for (ServiceReference ref : additionRefs) {
-                    FrameworkStatusAddition addition = (FrameworkStatusAddition) bundleContext.getService(ref);
-                    return addition.getServiceVariable(serviceID, name, client);
-                }
-            }
-            return FrameworkNodeStatus.SERVICE_STATUS_NOT_FOUND;
-        } catch (InvalidSyntaxException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void sortServiceReferences(ServiceReference[] additionRefs) {
-        Arrays.sort(additionRefs, new Comparator<ServiceReference>() {
-            @Override
-            public int compare(ServiceReference o1, ServiceReference o2) {
-                Object r1 = o1.getProperty(Constants.SERVICE_RANKING);
-                Object r2 = o2.getProperty(Constants.SERVICE_RANKING);
-
-                if (r1 instanceof Integer) {
-                    if (r2 instanceof Integer) {
-                        if (!r1.equals(r2)) {
-                            return ((Integer) r2) - ((Integer) r1);
-                        }
-                    } else {
-                        return -((Integer) r1);
-                    }
-                }
-
-                Long id1 = (Long) o1.getProperty(Constants.SERVICE_ID);
-                Long id2 = (Long) o2.getProperty(Constants.SERVICE_ID);
-                return (id1 > id2 ? -1 : 1);
-            }
-        });
-    }
-    */
 }
